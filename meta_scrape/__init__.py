de173e424f0024c3f459e0dfc40d110f92d3ffd7
Julien Machiels <julien.machiels@std.heh.be>
2017-04-05 10:56:16 +0200

    style(metascrape): fix all pep8 errors

--- meta_scrape/__init__.py
+++ meta_scrape/__init__.py
@@ -3,5 +3,6 @@ import os
 from yapsy.PluginManager import PluginManagerSingleton
 
 VERSION = "0.0.0"
-SCRAPERS_DIRECTORY = os.path.join(os.path.abspath(os.path.dirname(__file__)),'scrapers')
+SCRAPERS_DIRECTORY = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                  'scrapers')
 SCRAPER_MANAGER = PluginManagerSingleton.get()

76c0fd520991a3ecbbdbf2447b67f258735ea016
Julien Machiels <julien.machiels@protonmail.com>
2018-04-22 17:21:03 +0200

    fix(u15dvdinfo): add check for empty results page

--- meta_scrape/scrapers/u15dvdinfo.py
+++ meta_scrape/scrapers/u15dvdinfo.py
@@ -24,15 +24,17 @@ class U15DVDInfo(IPlugin):
         soup = BeautifulSoup(urllib.request.urlopen(self.U15_SEARCH_URL +
                                                     urllib.parse.quote(title)),
                              "html.parser")
-        result_list = soup.find_all("div", class_="entry")
-        if result_list:
-            for result in result_list:
-                result_title = result.find("h2") \
-                    .get_text().split('\n', 1)[0]
-                link = result.find("a").get('href')
-                self.results.append({"title": result_title, "link": link})
-        else:
-            pass
+        check_404 = soup.find("h1").get_text()
+        if "の検索結果：0件" not in check_404:
+            result_list = soup.find_all("div", class_="entry")
+            if result_list:
+                for result in result_list:
+                    result_title = result.find("h2") \
+                        .get_text().split('\n', 1)[0]
+                    link = result.find("a").get('href')
+                    self.results.append({"title": result_title, "link": link})
+            else:
+                pass
 
     def get_search_results(self, title):
         """Get search results for the title passed in parameter."""
@@ -61,7 +63,7 @@ class U15DVDInfo(IPlugin):
         if plot.endswith("\r\n"):
             plot = plot[:-2]
 
-        production_info_table = soup.find("table", class_="pro_info")\
+        production_info_table = soup.find("table", class_="pro_info") \
             .find_all("tr")
         for row in production_info_table:
             header = row.find("th").get_text()
